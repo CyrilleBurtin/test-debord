6b34fac6303840bed9361c8d53150fb60903c648
Cyrille <cyrille.burtin@me.com>
2025-05-14 15:54:34 +0200

    [feature] product page style

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { PRODUCT_MOCK } from "@/productsMock";
+import Link from "next/link";
 
 const ProductPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -15,13 +16,14 @@ const ProductPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   }
 
   return (
-    <div>
+    <div className="border border-amber-100 rounded-2xl container mx-auto p-10 flex flex-col gap-4">
       <h1>{product.title}</h1>
       <img src={product.image} alt={product.title} width={300} />
       <p>Price: ${product.price}</p>
       <p>{product.description}</p>
       <p>Category: {product.category}</p>
       <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
+      <Link href={`/`}><button className={"bg-blue-500 w-20 rounded-2xl cursor-pointer"}>home</button></Link>
     </div>
   );
 };
