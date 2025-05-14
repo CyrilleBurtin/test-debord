6debce42df9a48fe66c0cba641a11ffbb63ed43f
Cyrille <cyrille.burtin@me.com>
2025-05-14 15:56:09 +0200

    [feature] page grid is now responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
   const products: Product[] = await response.json();
 
   return (
-      <ul className="grid grid-cols-4 gap-4">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map(product => (
           <ProductCard key={product.id} {...product}/>
         ))}
